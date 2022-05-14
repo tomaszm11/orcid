f7c131305429fbead74d4b8731d7c86399411706
tomaszm11 <80033275+tomaszm11@users.noreply.github.com>
2022-05-14 14:14:27 +0200

    Update orcid1.py

--- orcid1.py
+++ orcid1.py
@@ -2,8 +2,8 @@ import urllib.request
 from xml.dom import minidom
 import sys
 import numpy as np
-# Zdaję sobie sprawę, że kod wykonuje się długo. Na pewno jest optymalniejszy sposób na wykonanie ćwiczenia ale niestety go nie znalazłem
-print('Proszę podać identyfikator orcid szukanej osoby: ')
+
+print('Please type the identifier of your scientist: ')
 orcid=str(input())
 
 with urllib.request.urlopen("https://pub.orcid.org/{}".format(orcid)) as f:
@@ -16,16 +16,16 @@ data=minidom.parseString(info)
 name=data.getElementsByTagName('personal-details:given-names')[0].firstChild.data
 surname=data.getElementsByTagName('personal-details:family-name')[0].firstChild.data
 #print(info)
-print('IMIE:',name)
-print('NAZWISKO:',surname)
+print('NAME:',name)
+print('SURNAME:',surname)
 fullname='{}_{}'.format(name,surname)
 
 
 txt=open('{}.txt'.format(fullname),'w')
 
 works = data.getElementsByTagName("activities:works")[0]
-print('PUBLIKACJE:')
-txt.write('PUBLIKACJE')
+print('PAPERS:')
+txt.write('PAPERS')
 txt.write("\n")
 for work in works.getElementsByTagName("activities:group"):
     title = work.getElementsByTagName("common:title")[0].firstChild.data
@@ -33,16 +33,16 @@ for work in works.getElementsByTagName("activities:group"):
     idtyp= work.getElementsByTagName("common:external-id-type")[0].firstChild.data
     idval=str(work.getElementsByTagName("common:external-id-normalized")[0].firstChild.data)
     url= work.getElementsByTagName("common:url")[0].firstChild.data
-    print('TYTUŁ: ',title)
-    tititle='TYTUŁ: {}'.format(title)
-    print('ROK WYDANIA: ',year)
-    yeyear='ROK WYDANIA: {}'.format(year)
+    print('TITLE: ',title)
+    tititle='TITLE: {}'.format(title)
+    print('YEAR OF PUBLISHMENT: ',year)
+    yeyear='YEAR OF PUBLISHMENT: {}'.format(year)
     print('AUTORZY: ')
     txt.write(tititle)
     txt.write("\n")
     txt.write(yeyear)
     txt.write("\n")
-    txt.write('AUTORZY: ')
+    txt.write('AUTHORS: ')
     txt.write("\n")
     if idtyp=='doi':
         req = urllib.request.Request("https://doi.org/{}".format(idval))
@@ -69,8 +69,8 @@ for work in works.getElementsByTagName("activities:group"):
             txt.write(author)
             txt.write("\n")
     elif idtyp!='doi' and idtyp!='arxiv':
-        print('brak informacji')
-        txt.write('brak informacji')
+        print('no info')
+        txt.write('no info')
         txt.write("\n")
 
     print(idtyp,':',idval)
